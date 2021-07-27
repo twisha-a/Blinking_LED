c27410d4e0ff18be23653b6a6bc5839ae688c435
twisha-a <66455753+twisha-a@users.noreply.github.com>
2021-07-27 08:06:10 +0530

    Blinking_LED
    coded in arduino file

---
+++ sketch_jul12a.ino
@@ -0,0 +1,13 @@
+void setup() {
+
+pinMode(D1, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+digitalWrite(D1, HIGH);
+delay(1000);
+digitalWrite(D1, LOW);
+delay(1000);
+
+}
